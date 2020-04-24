fb3fe7e914eaed550e621c6acfbf7dc8604a879f
Sushant <sushantbhrdwj20@gmail.com>
2020-04-24 08:46:09 +0530

    refactor : Change restart condition
    Restart condition checks 'no' instead of 'not yes'.

--- bikeshare.py
+++ bikeshare.py
@@ -226,7 +226,7 @@ def main():
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
 
-        if restart.lower() != 'yes':
+        if restart.lower() = 'no':
             break
 
 
